e66766ed5086ea079cacf2e9b90f0acd9f1c69b2
Stephane <Stephane@powershelldistrict.com>
2016-08-05 11:35:58 +0200

    Added Tattoo Configuration

---
+++ Runbooks/TattooServer.ps1
@@ -0,0 +1,13 @@
+﻿Configuration Configuration_Server2012R2 {
+    Node 'localhost' {
+        Registry TattooServer {
+           
+            Ensure = 'Present'
+            key = 'HKLM:\SOFTWARE\Policies\Innobit\'
+            ValueName = 'DSCControlType'
+            ValueData = 'Azure'
+            ValueType = 'String' 
+            
+        }
+    }
+}
